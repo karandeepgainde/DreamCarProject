e96e5d9c3dafbdce005479868384d3f7d0fd6673
Karandeep <Karandeep@DESKTOP-S7ET770>
2021-01-28 22:53:45 +1300

    Add project files.

---
+++ Data/Major_ProjectContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Major_Project.Models;
+
+namespace Major_Project.Data
+{
+    public class Major_ProjectContext : DbContext
+    {
+        public Major_ProjectContext (DbContextOptions<Major_ProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Major_Project.Models.Customer> Customer { get; set; }
+
+        public DbSet<Major_Project.Models.Staff> Staff { get; set; }
+
+        public DbSet<Major_Project.Models.Car> Car { get; set; }
+
+        public DbSet<Major_Project.Models.Store> Store { get; set; }
+
+        public DbSet<Major_Project.Models.Buy> Buy { get; set; }
+    }
+}
---
+++ Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Major_Project.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Car_Name { get; set; }
+        public string Car_Make { get; set; }
+        public DateTime Car_Date { get; set; }
+        public string Car_Price { get; set; }
+    }
+}
